8059449a38a748613148df7a1c688d68b1a13e67
miftahuljannahhh <mj5416386@gmail.com>
2025-04-17 11:39:49 +0700

    membuat void output

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -90,3 +90,14 @@ void mergeSort(int low, int high)
     }
 }
 
+void output()
+{
+    cout << "\nData setelah diurutkan (Merge Sort): ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+
